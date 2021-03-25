a32dca6cecd3fda50765155bfa0a425a2ebd6e48
Walter Barreiro <walterbarreiro97@gmail.com>
2021-03-25 10:42:24 -0300

    Creada la navegacion entre fragments usando BottomNavegationView y NavegationComponent

--- app/src/main/java/com/example/laspiedrasapp/MainActivity.java
+++ app/src/main/java/com/example/laspiedrasapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.laspiedrasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Para que funcione la navegacion entre fragments
+        BottomNavigationView bottomNavegationView = findViewById(R.id.bnv_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavegationView,navController);
+        // ----
+
     }
 }
---
+++ app/src/main/java/com/example/laspiedrasapp/WholesaleFragment.java
@@ -0,0 +1,30 @@
+package com.example.laspiedrasapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WholesaleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WholesaleFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wholesale, container, false);
+    }
+}
