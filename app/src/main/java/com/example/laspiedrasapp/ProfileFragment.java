f0d425a4e48025ab132bb50e64dd11e3437ac358
Walter Barreiro <walterbarreiro97@gmail.com>
2021-03-29 15:38:34 -0300

    Agregadas funcionalidades a los botones y menu del ProfileFragment

--- app/src/main/java/com/example/laspiedrasapp/ProfileFragment.java
+++ app/src/main/java/com/example/laspiedrasapp/ProfileFragment.java
@@ -2,23 +2,101 @@ package com.example.laspiedrasapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.example.laspiedrasapp.databinding.FragmentProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
+    private FirebaseAuth mAuth; // Para poder obtener el id del usuario
+    private FragmentProfileBinding binding; // Para usar View Binding
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true); // Para el menu
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        mAuth = FirebaseAuth.getInstance();// Inicializo el auth del usuario
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentProfileBinding.bind(view);// Inicializo el binding
+        String userId= mAuth.getCurrentUser().getUid(); // Obtengo el id del usuario logeado
+        // Extraigo los datos del usuario de firebase
+
+        // ----
+        // Para colocar los datos del usuario en la vista
+//        binding.name.setText("Nuevo nombre del usuario "+userId);
+        // ----
+        // Para el toolbar
+        binding.toolbar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()){
+                case R.id.item_add_commerce:
+                    Toast.makeText(getActivity(), "Agregr comercio",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.item_add_service:
+                    Toast.makeText(getActivity(), "Agregar servicio",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.item_conf_prof:
+                    Toast.makeText(getActivity(), "Conficurar",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.item_contact:
+                    Toast.makeText(getActivity(), "Contacto",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.item_about:
+                    Toast.makeText(getActivity(), "Acerca de",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.item_logout:
+                    Toast.makeText(getActivity(), "Salir",Toast.LENGTH_SHORT).show();
+                    return true;
+                default: return super.onOptionsItemSelected(item);
+
+            }
+        });
+        // -----
+        // Para ir a la tienda
+        binding.goBusiness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Ir a mi servicio",Toast.LENGTH_SHORT).show();
+            }
+        });
+        //-----
+        // Prar ir al servicio
+        binding.goCommerce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Ir a mi tienda",Toast.LENGTH_SHORT).show();
+            }
+        });
+        //-----
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null; // Para liberar memoria
     }
 }
