e783236f7649f433f76cd51bce10e20a84d0dfb4
Walter Barreiro <walterbarreiro97@gmail.com>
2021-04-16 20:13:05 -0300

    Impementado el tablayout del MainFragment y arregaldos unos layouts

--- app/src/main/java/com/example/laspiedrasapp/MainFragment.java
+++ app/src/main/java/com/example/laspiedrasapp/MainFragment.java
@@ -2,11 +2,17 @@ package com.example.laspiedrasapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.laspiedrasapp.databinding.FragmentMainBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,45 +20,17 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MainFragment extends Fragment {
-    // Todo lo que esta abajo vino por defecto al crear el fragment, comente lo que no es necesario
-/*
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // ToDo hacer el adapter del ViewPager2
+    private FragmentMainBinding binding;
+    private MainViewPagerAdapter mainViewPagerAdapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public MainFragment() {
         // Required empty public constructor
     }
-*/
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment MainFragment.
-     */
-    /*
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }*/
     }
 
     @Override
@@ -62,4 +40,34 @@ public class MainFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentMainBinding.bind(view);
+        initTabLayoutViewPager2();
+    }
+
+    private void initTabLayoutViewPager2() {
+        mainViewPagerAdapter = new MainViewPagerAdapter(getActivity().getSupportFragmentManager(),getLifecycle());
+        mainViewPagerAdapter.addFragment(new MainProductsFragment());
+        mainViewPagerAdapter.addFragment(new MainCommerceFragment());
+        mainViewPagerAdapter.addFragment(new MainBusinessFragment());
+        binding.vp2MainFragment.setAdapter(mainViewPagerAdapter);
+        new TabLayoutMediator(binding.tlMainFragment, binding.vp2MainFragment, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0:
+                        tab.setText("Productos");
+                        break;
+                    case 1:
+                        tab.setText("Comercios");
+                        break;
+                    case 2:
+                        tab.setText("Servicios");
+                        break;
+                }
+            }
+        }).attach();
+    }
 }
