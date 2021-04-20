9e884a87e1324a7fc6ac5e4f050e993a9c6ebc22
Walter Barreiro <walterbarreiro97@gmail.com>
2021-04-20 17:18:12 -0300

    Implementado el MainProductsFragment.

--- app/src/main/java/com/example/laspiedrasapp/MainProductsFragment.java
+++ app/src/main/java/com/example/laspiedrasapp/MainProductsFragment.java
@@ -2,63 +2,120 @@ package com.example.laspiedrasapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.example.laspiedrasapp.databinding.FragmentMainProductsBinding;
+import com.example.laspiedrasapp.models.CommerceProductModel;
+import com.example.laspiedrasapp.models.CommerceProductModel;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainProductsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+
 public class MainProductsFragment extends Fragment {
+    FragmentMainProductsBinding binding; //Binding (para simplificar)
+    CommerceMainProductAdapter adaptador;
+    RecyclerView RV; // RecyclerView usado con FireBaseAdapter
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MainProductsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainProductsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainProductsFragment newInstance(String param1, String param2) {
-        MainProductsFragment fragment = new MainProductsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    public MainProductsFragment() { } // Required empty public constructor
     @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_products, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_products, container, false);
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentMainProductsBinding.bind(view);// Inicializo el binding
+
+        RV = (RecyclerView) binding.rvMainProdFrag;
+        //RV.setLayoutManager(new LinearLayoutManager(getContext()));
+        RV.setLayoutManager(new GridLayoutManager(getContext(),3));
+
+        listarProductosCommerce();
+        filtrarBusqueda();
+    }
+
+    private void listarProductosCommerce() {
+        FirebaseRecyclerOptions<CommerceProductModel> options =
+                new FirebaseRecyclerOptions.Builder<CommerceProductModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference("shops_product"), CommerceProductModel.class)
+                        .build();
+        adaptador = new CommerceMainProductAdapter(options);
+        adaptador.startListening();
+        RV.setAdapter(adaptador);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adaptador.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adaptador.stopListening();
+    }
+
+    private void filtrarBusqueda() {
+        binding.searchProductFrag.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                buscar(query);
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                //buscar(s);
+                return false;
+            }
+        });
+
+        binding.searchProductFrag.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                listarProductosCommerce();
+                return false;
+            }
+        });
+    }
+    private void buscar(String s)
+    {
+        FirebaseRecyclerOptions<CommerceProductModel> options =
+                new FirebaseRecyclerOptions.Builder<CommerceProductModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference("shops_product").orderByChild("name").startAt(s).endAt(s+"\uf8ff"), CommerceProductModel.class)
+                        .build();
+
+        adaptador=new CommerceMainProductAdapter(options);
+        adaptador.startListening();
+        RV.setAdapter(adaptador);
+    }
+
+
+
+
+
+
+
+
 }
