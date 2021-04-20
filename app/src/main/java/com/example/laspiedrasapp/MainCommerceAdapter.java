86cd3931d6153363752a35c29d2979c7922888ab
Walter Barreiro <walterbarreiro97@gmail.com>
2021-04-20 09:15:41 -0300

    Funcionando el buscador del MainCommerceFragment. Falta mejorar el diseño

---
+++ app/src/main/java/com/example/laspiedrasapp/MainCommerceAdapter.java
@@ -0,0 +1,55 @@
+package com.example.laspiedrasapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.laspiedrasapp.models.CommerceModel;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class MainCommerceAdapter extends FirebaseRecyclerAdapter<CommerceModel, MainCommerceAdapter.MainCommerceViewHolder> {
+    private Context context;
+
+    public MainCommerceAdapter(@NonNull FirebaseRecyclerOptions<CommerceModel> options, Context context)
+    {
+        super(options);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MainCommerceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(parent.getContext()) .inflate(R.layout.item_main_commerce, parent, false);
+        return new MainCommerceAdapter.MainCommerceViewHolder(view);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull MainCommerceViewHolder holder, int position, @NonNull CommerceModel model) {
+        holder.firstname.setText(model.getName());
+        Glide.with(context).load(model.getBanner_url()).into(holder.banner);
+
+
+    }
+
+    class MainCommerceViewHolder extends RecyclerView.ViewHolder {
+        TextView firstname;
+        ImageView banner;
+        public MainCommerceViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+            firstname = itemView.findViewById(R.id.tvItemMainCommerceName);
+            banner = itemView.findViewById(R.id.ivItemMainCommerceBanner);
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/laspiedrasapp/MainCommerceFragment.java
+++ app/src/main/java/com/example/laspiedrasapp/MainCommerceFragment.java
@@ -2,63 +2,111 @@ package com.example.laspiedrasapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainCommerceFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.laspiedrasapp.databinding.FragmentMainCommerceBinding;
+import com.example.laspiedrasapp.models.CommerceModel;
+import com.example.laspiedrasapp.models.ProfileProductModel;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainCommerceFragment extends Fragment {
+    private FirebaseAuth mAuth; // Para poder obtener el id del usuario
+    private DatabaseReference mDatabase; // Para extraer los datos de firebase
+    private FragmentMainCommerceBinding binding;
+    private MainCommerceAdapter mainCommerceAdapter;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public MainCommerceFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainCommerceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainCommerceFragment newInstance(String param1, String param2) {
-        MainCommerceFragment fragment = new MainCommerceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mAuth = FirebaseAuth.getInstance();// Inicializo el auth del usuario
+        mDatabase = FirebaseDatabase.getInstance().getReference("shops"); // Inicializo firebase
         return inflater.inflate(R.layout.fragment_main_commerce, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentMainCommerceBinding.bind(view);
+
+        initRecyclerView();
+        searchViewListeners();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mainCommerceAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mainCommerceAdapter.stopListening();
+    }
+
+    private void initRecyclerView(){
+        binding.rvMainCommerce.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL,false));
+        FirebaseRecyclerOptions<CommerceModel> options = new FirebaseRecyclerOptions.Builder<CommerceModel>()
+                .setQuery(mDatabase, CommerceModel.class).build();
+        mainCommerceAdapter = new MainCommerceAdapter(options,getContext());
+        binding.rvMainCommerce.setAdapter(mainCommerceAdapter);
+    }
+    private void searchViewListeners() {
+        binding.svMainCommerce.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchQuery(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        binding.svMainCommerce.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                searchQuery("");
+                return false;
+            }
+        });
+
+    }
+    private void searchQuery(String s) {
+        binding.rvMainCommerce.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL,false));
+        FirebaseRecyclerOptions<CommerceModel> options = new FirebaseRecyclerOptions.Builder<CommerceModel>()
+                .setQuery(mDatabase.orderByChild("name").startAt(s).endAt(s+"\uf8ff"), CommerceModel.class).build();
+        mainCommerceAdapter = new MainCommerceAdapter(options,getContext());
+        mainCommerceAdapter.startListening();
+        binding.rvMainCommerce.setAdapter(mainCommerceAdapter);
     }
 }
